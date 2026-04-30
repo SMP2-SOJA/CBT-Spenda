92d7362a902720a4a4a83e7bdc8d40807461a55c
spendasoja <rahmath23@admin.smp.belajar.id>
2026-04-30 19:42:33 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'cbt-spenda-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/logo-sekolah.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch dari Cache saat Offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
